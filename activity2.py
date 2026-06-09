83721c591c556f99b83a2b47f99ead5eeebed707
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-06-09 11:36:14 +0530

    Add files via upload

--- activity2.py
+++ activity2.py
@@ -1,8 +1,12 @@
-x = 5
-y ="John"
-print(x)
-print(y)
-codingal = "codingal"
-print ("codingal")
-name = input ("enter your name")
-print("\nhello", name, "\nwelcome to codingal")
+#Taking total amount as input from user
+Amount = int(input("Please enter amount for withdraw: "))
+
+#Calculating the number of notes from different denominations
+note1 = Amount//100
+note2 = (Amount%100)//50
+note3 = ((Amount %100)%50)//10
+
+print("notes of 100 rupee", note1)
+print("notes of 50 rupee", note2)
+print("notes of 10 rupee", note3)
+
