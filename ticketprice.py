e8c562736fd5c7d7dac83c420fd882e0f57d43ad
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-06-09 11:49:13 +0530

    Add files via upload

---
+++ ticketprice.py
@@ -0,0 +1,11 @@
+print("Movie ticket calculator")
+movie_name = input("Enter movie name: ")
+ticket_price = int(input("Enter Ticket price: "))
+tickets = int(input("How many tickets? : "))
+
+Total_bill = ticket_price * tickets
+
+print("movie name:", movie_name)
+print("ticket price:", ticket_price)
+print("number of tickets:", tickets)
+print("Total bill:",Total_bill)
