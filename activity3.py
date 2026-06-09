7dd6af0e301cb5a2070e4df55aac127c44b2cbe0
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-06-09 11:36:48 +0530

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,3 +1,13 @@
-import keyword
-print("python keywords are...\n")
-print(keyword.kwlist)
+#take marks as input from user
+print("Enter marks obtained in each subject: ")
+math = int(input("maths: "))
+science = int(input("science: "))
+english = int(input("english: "))
+hindi = int(input("hindi: "))
+
+#Calculating the percentage of marks
+sum = math+science+english+hindi
+print("sum of math, science, english and hindi = ", sum)
+
+perc = (sum/400) *100
+print("percentage = ", perc)
