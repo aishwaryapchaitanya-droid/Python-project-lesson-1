b60586b02141e29ccd13c9a83324c0db296d817b
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-06-09 11:35:36 +0530

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,5 +1,15 @@
-print("welcome to the world of programming")
-print(4)
-print("hello python \n ")
-print("hello",5)
-print("welcome to", end = "*")
+#storing values
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 96
+tree5 = 11
+
+#Finding sum of all 5 trees
+sum = tree1 + tree2 + tree3 + tree4 + tree5
+print("sum of all 5 trees is:", sum)
+
+#Finding average 
+average = sum/5
+print("the average of all 5 tress is: ",average)
+
