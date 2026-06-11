a809aaf5db0272c2a7788e175d6ff368cc925471
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-06-11 11:35:26 +0530

    Add files via upload

--- activity2.py
+++ activity2.py
@@ -1,12 +1,9 @@
-#Taking total amount as input from user
-Amount = int(input("Please enter amount for withdraw: "))
+actual_cost = float(input("Please Enter the actual product price: "))
+sale_amount = float(input("Please Enter the Sale amount: "))
 
-#Calculating the number of notes from different denominations
-note1 = Amount//100
-note2 = (Amount%100)//50
-note3 = ((Amount %100)%50)//10
+if (sale_amount > actual_cost ):
+    amount = sale_amount - actual_cost
+    print("total profit = {0}", (amount))
 
-print("notes of 100 rupee", note1)
-print("notes of 50 rupee", note2)
-print("notes of 10 rupee", note3)
-
+else:
+    print('No profit!!')
