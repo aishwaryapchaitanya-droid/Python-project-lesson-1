3f5600b7eedd8ee6e2b17d5710fcd2b3e3669a8f
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-06-12 11:40:41 +0530

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,5 +1,22 @@
-num = int(input("Enter number: "))
-if num > 0:
-    print(num, "is a positive number")
+a = 1
+b = 12
+c = 0 
+if a and b and c: 
+    print("All the numbers have boolean value as true")
+else:
+    print("Atleast one number has boolean value as False")
 
+a = 10
+b = -10
+c = 0
 
+if a>0 or b>0: 
+    print("Either of the number is greater than 0 ")
+else:
+    print("No number is greater than 0")
+
+if b>0 and c>0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
+    
