4ca2065d5310f31d0c02ccdfcec6c828ebbba35d
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-06-15 11:48:39 +0530

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,20 +1,44 @@
-height = float(input("Enter your height in cm: "))
-weight = float (input("Enter your weight in kg: "))
+print("Enter marks obtained in 5 subjects")
 
-BMI = weight / (height/100)**2
+markone = int(input("Maths: "))
+marktwo = int(input("Science: "))
+markthree = int(input("Social Studies: "))
+markfour = int(input("English: "))
+markfive = int(input("French: "))
 
-print("your BMI is: ", BMI)
+total = markone + marktwo + markthree + markfour + markfive 
+avg = int(total / 5)    
+validrange = range(0,101)
 
-if BMI <= 18.4:
-    print("you are under weight.")
-elif BMI <= 24.9:
-    print("You qre healthy.")
-elif BMI <= 29.9:
-    print("You are over weight")
-elif BMI <= 34.9:
-    print("you are severely over weight")
-elif BMI <= 39.9:
-    print("you are obese.")
+if avg not in validrange:
+    print("Invalid input!")
+
+elif avg in range (91,101):
+    print("Your grade is A1")
+
+elif avg in range (81,91):
+    print("your grade is A2")
+
+elif avg in range (71,81):
+    print("your grade is B1")
+
+elif avg in range (61,71):
+    print("your grade is B2")
+
+elif avg in range(51,61):
+    print("your grade is c1")
+
+elif avg in range(41,51):
+    print("your garde is C2")
+
+elif avg in range(33,41):
+    print("your grade is D")
+
+elif avg in range(21,33):
+    print("your garde is E1")
+
+elif avg in range(11,21):
+    print("your grade is E2")
+
 else:
-    print("you are overly obese")
-    
+    print("Fail")
