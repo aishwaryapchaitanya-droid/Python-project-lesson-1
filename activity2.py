5be83a5d279f1e9c05364d29043b86a897127df3
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-06-16 11:49:35 +0530

    Add files via upload

--- activity2.py
+++ activity2.py
@@ -1,31 +1,8 @@
-#program to check the application of not operator
-
-a = 10 
-b = 12
-c = 12
-
-#not is used here to reverse the result of (a==b)
-print(not(a==b))
-
-#not is used here to reverse the result of (b==c)
-print(not( b==c))
-
-a = "python"
-b = "coding"
-
-#not is used here to check that a is not equal to b 
-if not (a==b):
-    print(a, 'and',b, 'are different')
-
-a = 4
-h = 5
-
-#not is used here to reverse the result of comparing both conditions 
-if not ((a==1)== (b==5)):
-    print("Hello")
-
-a = int(input("Enter a number: "))
-
-#not is used here to check that the number is not equal
-if not (a % 2 == 0):
-    print("a is an odd number")
+print("Eneter a number (numerator): ")
+numn = (int(input()))
+print("Enter a number (denominator): ")
+numd = (int(input()))
+if numn%numd == 0:
+    print("\n" + str(numn)+ "is divisible by" + str(numd))
+else:
+    print("\n"+ str(numn)+ "is not divisible by" + str(numd))
