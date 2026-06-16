5d17255523cdd19e727653546eb19166fb95e65a
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-06-16 11:48:52 +0530

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,17 +1,15 @@
-x = 5
-if(type(x) is int):
-    print("true")
+v = 4
+w = 5
+x = 8
+y = 2 
+z = 0
+z = (v+w) * x/y
+print("value of (v+w) * x/y is ", z)
+
+name = "Alex"
+age = 0 
+if name == "Alex" or name == "John" and age>= 2:
+    print("Hello, Welcome!")
 else:
-    print("false")
-x = 5.5
-if (type(x) is not float):
-    print("true")
-else:
-    print("false")
-x = 20
-y = 20
-if (x is y):
-    print("x & y SAME IDENTITY")
-y = 30
-if ( x is not y):
-    print("x & y have DIFFERENT IDENTITY")
+    print("Goodbye!")
+
