8676d0f032aae1eecefffb5ccce676f20c385135
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-06-16 11:50:29 +0530

    Add files via upload

--- activity4.py
+++ activity4.py
@@ -1,8 +1,32 @@
-number = int(input("Enter a number: "))
-print("Number to be checked: ", number)
+a = int(input("Enter a value: "))
+b = int(input("Enter value 2: "))
+c = int(input("Enter value 3: "))
 
-if number%2==0:
-    print("This is an even number")
+avg = (a+b+c)/3
+print("avg = ",avg)
+
+if avg>a and avg>b and avg>c:
+    print("%d is higher than %d,%d,%d" % (avg, a, b, c))
+
+elif avg>a and avg>b:
+    print("%dis higher than %d,%d" % (avg, a, b, ))
+
+elif avg>a and avg>c:
+    print("%dis higher than %d,%d" % (avg, a, c, ))
+
+elif avg>b and avg>c:
+    print("%dis higher than %d,%d" % (avg, b, c))
+
+elif avg> a:
+    print("%dis higher than %d,%d" % (avg, a))
+
+elif avg> b:
+    print("%dis higher than %d,%d" % (avg, b))
+
+elif avg>c:
+    print("%dis higher than %d,%d" % (avg, c))
 
 else:
-    print("This is an odd number")
+    print("Invalid input")
+
+    
