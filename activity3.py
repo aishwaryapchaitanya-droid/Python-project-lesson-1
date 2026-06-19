65fb639f330af041f976d455c42de712cba7c17e
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-06-19 11:58:49 +0530

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,16 +1,40 @@
-mean1 = 38
-wrongnumber = 36
-correctnumber = 56
-totalnumber = 40
-#sum of 40 number
-sum = mean1*totalnumber
-print("the sum of 40 numbers is: ", sum)
+print("Select your ride: ")
+print("1. Two wheeler ")
+print("2. Four wheeler ")
 
-#correct sum of these numebers
-num2 = sum- ((wrongnumber)-(correctnumber))
-print("sum- ((wrongnumber)-(correctnumber)) = ", num2)
+choice = int (input("Enter your choice: "))
 
-#the correct mean
-mean2 = num2/totalnumber
-print(mean2)
+if (choice == 1):
+    print("What type of two wheeler? ")
+    print("1. Scooty\n")
+    print("2. Scooter\n")
+
+    choice2 = int(input("Enter your choice 2: "))
+    if choice2==1:
+        print("You have seleted scooty")
+    
+    else:
+        print("You have selected scooter")
+
+elif(choice == 2):
+    print(("What type of four wheeler? "))
+    print("1.Sedan")
+    print("2.XUV")
+    choice3 = int(input("enter your choice 3: "))
+
+    if choice3 ==1:
+        print("You have selected sedan")
+    else: 
+        print("you have slected XUV")
+
+else:
+    print("wrong choice")
+
+
+
+
+
+
+
+
 
