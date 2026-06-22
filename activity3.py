f64fad20c591d23e4b1cb20032687dd8d7086c9a
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-06-22 11:39:03 +0530

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,40 +1,9 @@
-print("Select your ride: ")
-print("1. Two wheeler ")
-print("2. Four wheeler ")
+#input number greater than 1
+n = int(input("Enter a value of n: "))
 
-choice = int (input("Enter your choice: "))
+#printnumbers from 0 to 1
+print("number from",n,"to 1 are")
 
-if (choice == 1):
-    print("What type of two wheeler? ")
-    print("1. Scooty\n")
-    print("2. Scooter\n")
-
-    choice2 = int(input("Enter your choice 2: "))
-    if choice2==1:
-        print("You have seleted scooty")
-    
-    else:
-        print("You have selected scooter")
-
-elif(choice == 2):
-    print(("What type of four wheeler? "))
-    print("1.Sedan")
-    print("2.XUV")
-    choice3 = int(input("enter your choice 3: "))
-
-    if choice3 ==1:
-        print("You have selected sedan")
-    else: 
-        print("you have slected XUV")
-
-else:
-    print("wrong choice")
-
-
-
-
-
-
-
-
-
+#loop to print numbers
+for i in range (n,0,-1):
+    print(i)
