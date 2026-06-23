85e8ea8b450ed4f40c01a2612b4c226a5ce335b2
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-06-23 11:25:52 +0530

    Add files via upload

--- activity2.py
+++ activity2.py
@@ -1,10 +1,3 @@
-#INput a word or a sentence
-string = input("PLease enter your own string: ")
-
-string2 = ('')
-#loop for printing in reverse
-for i in string:
-    string2 = i+string2
-
-print("\n the original string = ", string)
-print("\nThe reversed string = ", string2)
+i = 0
+while i<=0:
+    print("I WILL RUN FOREVER")
