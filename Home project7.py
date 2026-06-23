2dba33a05dbdf3f27575b6b65689cd72b5be7ff9
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-06-23 08:54:01 +0530

    Add files via upload

--- Home project7.py
+++ Home project7.py
@@ -1,3 +1,11 @@
 x = input("Enter a character: ")
 ASCII_Value = ord(x)
 print("The ASCII vaue of", x, "is", ASCII_Value) 
+if x in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefjhijklmnopqrstuvwxyz":
+    print("Alphabet")
+
+elif x in "1234567890":
+    print("Digit")
+
+else:
+    print("Special character")
