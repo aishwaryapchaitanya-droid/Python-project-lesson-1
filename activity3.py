09454b20b80f3e585f91e8a8743d6cfbac8bece6
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-06-29 11:20:05 +0530

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,16 +1,10 @@
-#take input from user
-num = int(input("Enter a number: "))
-#store original number
-original = sum 
-#initialize sum
-total = 0 
-#find dum of cubes of digits
-while num>0:
-    digit = num % 10  #get last digit
-    total = total + digit**3#add cube of digit
-    num = num//10 #remove last digit
-#check armstrong number
-if original == total: 
-    print("is is an Armstrong number", original)
-else: 
-    print("is is not an Armstrong number", original)
+num = input("Enter a number: ")
+length = len(num)
+start = (length  - 1)//2
+end = length//2
+product = 1
+for i in range (start, end + 1):
+    for j in num [i]:
+        product = product * int (j)
+
+print("product of middle digit(s) = ", product)
