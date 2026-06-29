b38a3d1ba261d03fc52b7bd1546c655e6bcc9e9e
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-06-29 11:48:24 +0530

    Add files via upload

--- activity2.py
+++ activity2.py
@@ -1,3 +1,8 @@
-i = 0
-while i<=0:
-    print("I WILL RUN FOREVER")
+rows = int(input("Please enter the total number of rows : "))
+number = 1
+print("Floyd's triangle")
+for i in range (1, rows+1):
+    for j in range (1, i + 1):
+        print(number, end = ' ')
+        number = number + 1
+    print()
