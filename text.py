2bf62884c15d4873d64d8b873177402c42aed13f
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-06-30 11:55:58 +0530

    Add files via upload

---
+++ text.py
@@ -0,0 +1,114 @@
+secret = 27
+print("The secret number is between 0 to 100")
+
+#Guess 1
+guess1 = int(input("Guess1 : "))
+
+if guess1 == 27:
+    print("Correct guess")
+elif 17 <=guess1 <= 37: 
+    print("hot")
+elif 17 <= guess1 <= 47:
+    print("Warm")
+elif 10<= guess1 <= 60:
+    print("Cold")
+elif 0 <= guess1 <= 100:
+    print("Ice cold")
+
+i = 5
+for i in range (6):
+    i - 1
+print(i-1,"hearts remaining")
+if i == 0:
+    print("You lost")
+
+
+#Guess2
+guess2 = int(input("Guess2 : "))
+
+if guess2 == 27:
+    print("Correct guess")
+elif 17 <=guess2 <= 37: 
+    print("hot")
+elif 17 <= guess2 <= 47:
+    print("Warm")
+elif 10<= guess2 <= 60:
+    print("Cold")
+elif 0 <= guess2 <= 100:
+    print("Ice cold")
+
+i = 4
+for i in range (5):
+    i - 1
+print(i-1,"hearts remaining")
+if i == 0:
+    print("You lost")
+
+
+#Guess3
+guess3 = int(input("Guess3 : "))
+
+if guess3 == 27:
+    print("Correct guess")
+elif 17 <=guess3 <= 37: 
+    print("hot")
+elif 17 <= guess3 <= 47:
+    print("Warm")
+elif 10<= guess3 <= 60:
+    print("Cold")
+elif 0 <= guess3 <= 100:
+    print("Ice cold")
+
+i = 3
+for i in range (4):
+    i - 1
+print(i-1,"hearts remaining")
+if i == 0:
+    print("You lost")
+
+
+#Guess 4
+guess4 = int(input("Guess4 : "))
+
+if guess4 == 27:
+    print("Correct guess")
+elif 17 <=guess4 <= 37: 
+    print("hot")
+elif 17 <= guess4 <= 47:
+    print("Warm")
+elif 10<= guess4 <= 60:
+    print("Cold")
+elif 0 <= guess4 <= 100:
+    print("Ice cold")
+
+i = 2
+for i in range (3):
+    i - 1
+print(i-1,"hearts remaining")
+if i == 0:
+    print("You lost")
+
+
+#Guess 5
+guess5 = int(input("Guess5 : "))
+
+if guess5 == 27:
+    print("Correct guess")
+elif 17 <=guess5 <= 37: 
+    print("hot")
+elif 17 <= guess5 <= 47:
+    print("Warm")
+elif 10<= guess5 <= 60:
+    print("Cold")
+elif 0 <= guess5 <= 100:
+    print("Ice cold")
+
+i = 1
+for i in range (2):
+    i - 1
+print(i-1,"hearts remaining")
+if i == 0:
+    print("You lost")
+
+
+
