3f74916fe4add0f1829d1ca68f1b4eff3744d389
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-07-02 11:34:09 +0530

    Add files via upload

--- activity2.py
+++ activity2.py
@@ -1,8 +1,7 @@
-rows = int(input("Please enter the total number of rows : "))
-number = 1
-print("Floyd's triangle")
-for i in range (1, rows+1):
-    for j in range (1, i + 1):
-        print(number, end = ' ')
-        number = number + 1
-    print()
+def weather_Condition():
+    print("The weather is pleasent in", spring)
+    print("The weather is same in", autumn)
+
+spring = "autumn"
+autumn = spring 
+weather_Condition()
