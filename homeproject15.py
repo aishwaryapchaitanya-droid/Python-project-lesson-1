0ce7b81af096a2ec5f5990e6b5f4964735a0e7bc
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-07-02 15:15:42 +0530

    Add files via upload

---
+++ homeproject15.py
@@ -0,0 +1,6 @@
+def circumference (radius):
+    circumference = 2*3.14*radius
+    print("circumference of circle = ", circumference)
+
+radius = float(input("Enter the radius of the cirule: "))
+circumference(radius)
