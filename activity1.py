dedd2e0282e2d827d7dd7276bf861fcde978103a
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-07-03 11:30:21 +0530

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,4 +1,6 @@
-def well_wishes():
-    print("Hello")
-    print("How are you?")
-well_wishes()
+def total_calc(bill_amount, tip_perc):
+    total = bill_amount*(1+0.01*tip_perc)
+    total = round(total, 2)
+    print(f"Please pay ${total}")
+
+total_calc(150,2)
