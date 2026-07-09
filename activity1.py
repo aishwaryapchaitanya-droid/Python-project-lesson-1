3702b6c96a19b215ce5c61137a9d3fc54c345fe9
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-07-09 11:13:49 +0530

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,6 +1,16 @@
-try:
-    number = int(input("Enter a number: "))
-    print("The number entered is ", number)
+import random
+playing = True
+number = str(random.randint(0,9))
 
-except ValueError as ex:
-    print("Exception", ex)
+print("I will generate a number from 0 to 9 and you have to guess the number one digit at a time")
+print("The game ends when you get one hero")
+
+while playing:
+    guess = input("Give me you best guess: \n")
+    if number == guess:
+        print("You win the game!")
+        print("The number was", number)
+        break
+    else:
+        print("Your guess isn't quite right, try again\n")
+        
