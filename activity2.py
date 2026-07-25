dd12a88e906448033f5f51f72037bf8db226bc21
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-07-25 11:53:18 +0530

    Add files via upload

--- activity2.py
+++ activity2.py
@@ -1,32 +1,11 @@
-import random 
-while True:
-    user_action = input("Enter a choice(rock, paper, scissors): ")
-    possible_action = ["rock", "paper", "scissors"]
-
-    computer_action = random.choice(possible_action)
-    print(f"\nYou chose{user_action}, computer chose{computer_action}.\n ")
-
-    if user_action == computer_action:
-        print("Both players selected {user_action}. It is a tie")
-
-    elif user_action == "rock":
-        if computer_action == "scissors":
-            print("Rock smashes scissors. You win!")
-        else:
-            print("Paper covers rock. You lose!")
-
-    elif user_action == "paper":
-        if computer_action == "rock":
-            print("Paper covers rock. You win!")
-        else:
-            print("Scissors cuts paper. You lose!")
-    
-    elif user_action == "scissors":
-        if computer_action == "paper":
-            print("scissors cuts paper. You win!")
-        else:
-            print("Rock smashes scissors. You lose!")
-    
-    play_again = input("Play Again? (Y/N): ")
-    if play_again != "Y":
-        break
+def match_words(words):
+    ctr = 0
+    lst = []
+    for word in words:
+        if len (word)>1 and word[0] == word[-1]:
+            ctr +=1
+            lst.append(word)
+    print("List of words with first and last characters same\n", lst)
+    return ctr
+count = match_words(['abc', 'cfc', 'xyz', '1221'])
+print("Number of words having fist and last character same: ", count)
