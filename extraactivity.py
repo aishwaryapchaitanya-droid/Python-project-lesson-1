d187960120ff43bc70179d5a55f165897080a618
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-07-25 12:06:08 +0530

    Add files via upload

--- extraactivity.py
+++ extraactivity.py
@@ -1,15 +1,25 @@
-print("🍕 Welcome to pizza shope!")
-try:
-    quantity = int(input("Enter the number of pizzas: "))
-    price = int(input("Enter the price of one pizza: "))
-    total = quantity*price
+name = input ("Enter customer name: ")
+customer_id = name[:3].upper()+name [-1].upper()
+prices = [120, 250, 80, 60, 150]
+print("\nOriginal price list: ", prices)
 
-except ValueError:
-    print("PLease enter numbers only.")
+total = 0 
+for price in prices:
+    total +=price
 
-else:
-    print("Total bill = ", total)
-    print("Thank you for your order! ")
+average = total/len(prices)
+prices.sort()
 
-finally:
-    print("Visit Again!")
+smallest = prices[0]
+largest = prices[-1]
+reverse_prices = prices[::-1]
+
+bill_text = str(total)
+
+print("\n========== SHOPPING SUMMARY ==========")
+print("customer id: ", customer_id)
+print("total bill: ", bill_text)
+print("average item price: ", average)
+print("cheapest item: ", smallest)
+print("Costliest item: ", largest )
+print("reverse sorted prices: ", reverse_prices)
