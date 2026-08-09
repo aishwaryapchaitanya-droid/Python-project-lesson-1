c1cac0901ab24e5f5e984aa05f09d30c65e8cf1e
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-08-09 12:00:05 +0530

    Add files via upload

--- Activity2.py
+++ Activity2.py
@@ -1,16 +1,12 @@
-def palind(r):
-    e = len(r) -1
-    s = 0 
-    while (s<e):
-        if (r[s]!= r[e]):
-            return False
-        s+=1
-        e-=1
-    return True
+test_dict = {'codingal': 2, 'is' : 2, 'best' : 2, 'for' : 2, 'coding' : 1, 'Interesting': 1}
 
-r = (1,2,3,3,2,1)
+print("The original dictionary : " +str(test_dict))
 
-if (palind(r)):
-    print("The tuple is Flip-Flop")
-else:
-    print("The tuple is not Flip -Flop")
+k = 1
+
+res = 0 
+for key in test_dict:
+    if test_dict[key] == k:
+        res = res + 1
+
+print("Frequency of K is : " + str(res))
