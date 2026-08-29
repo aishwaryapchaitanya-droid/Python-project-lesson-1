6dd46e710e09b611e1e6275f4b6fe725fc587b33
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-08-29 12:06:07 +0530

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,18 +1,13 @@
-class parrot:
-    species = "bird"
+class pair_elements:
+    def twoSum(self, nums, target):
+        lookup =  {}
 
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-blu = parrot("blu", 10)
-woo = parrot("woo", 15)
-
-print("blu is {}".format(blu.species))
-print("woo is {}".format(woo.species))
-
-print("{} is {} years old".format(blu.name,blu.age))
-print("{} is {} years old".format(woo.name,woo.age))
+        for i, num in enumerate(nums):
+            if target - num in lookup:
+                return(lookup[target-num], i)
+            lookup [num]= i
 
 
-
+value = int(input("Enter sum for which you want to make this search: " ))
+print("index1 = %d, index2 = %d" %
+pair_elements(). twoSum((10,20,30,40,50,60,70), value))
