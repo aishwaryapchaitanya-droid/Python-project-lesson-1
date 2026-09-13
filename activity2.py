11c8c716ee06cec1ec988fc8cd8a346a67b23977
aishwaryapchaitanya-droid <aishwaryap.chaitanya@gmail.com>
2026-09-13 11:55:15 +0530

    Add files via upload

--- activity2.py
+++ activity2.py
@@ -1,33 +1,76 @@
-from abc import ABC, abstractmethod
+import pygame
 
-class Animal(ABC):
-    def move(self):
-        pass
+pygame.init()
 
-class Human(Animal):
-    def move(self):
-        print("I can walk and run")
+#screen dimentions
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
 
-class Snake(Animal):
-    def move(self):
-        print("I can crawl")
+#Create Screen
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Penguin with shapes")
 
-class dog(Animal):
-    def move(self):
-        print("I can bark")
+#Colors
+SKY_BLUE = (135, 206, 235)
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+ORANGE = (255, 165, 0)
+LIGHT_BLUE = (200,  230,  255)
 
-class Lion(Animal):
-    def move(self):
-        print("I can roar")
 
-R = Human()
-R.move()
+clock = pygame.time.Clock()
+running = True
 
-K = Snake()
-K.move()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-R = dog()
-R.move()
+    #Background
+    screen.fill(SKY_BLUE)
 
-K = Lion()
-K.move()
+    #Snow ground
+    pygame.draw.rect(screen, WHITE, (0,380,500,120))
+
+    #Ice block
+    pygame.draw.rect(screen, LIGHT_BLUE(50,330,100,50))
+    pygame.draw.rect(screen, LIGHT_BLUE(350,320,120,60))
+
+    #Penguin Body
+    pygame.draw.ellipse(screen, BLACK(180,150,140,200))
+
+    #penguin tummy
+    pygame.draw.ellipse(screen, WHITE(205,190,90,130))    
+
+    #Penguin head
+    pygame.draw.circle(screen, BLACK(250,130),60)
+
+    #Eyes
+    pygame.draw.circle(screen, WHITE(230,115),10)
+    pygame.draw.circle(screen, WHITE(270,155),10)
+
+    pygame.draw.circle(screen, BLACK(230,155),4)
+    pygame.draw.circle(screen, BLACK(270,155),4)
+
+    #Beak
+    pygame.draw.polygon(screen, ORANGE,
+                        [(250, 130), (235, 145), (265, 145)])
+
+    # Feet
+
+    pygame.draw.ellipse(screen, ORANGE, (205, 335, 35, 15))
+    pygame.draw.ellipse(screen, ORANGE, (260, 335, 35, 15))
+
+    # Wings
+    pygame.draw.ellipse(screen, BLACK, (145, 190, 45, 110))
+    pygame.draw.ellipse(screen, BLACK, (310, 190, 45, 110))
+
+    # Text
+    font = pygame.font.Font(None, 40)
+    text = font.render("Hello World!", True, BLACK)
+    screen.blit(text, (170, 430))
+
+    pygame.display.update()
+    clock.tick(30)
+
+pygame.quit()
